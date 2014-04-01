231a4a1fe08190ab4449ba410d66ee2690cc86b4
Leah Brann <lbrann@gmail.com>
2014-04-01 16:48:25 -0700

    Activity and Category models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ITSwellness::Application.config.secret_key_base = 'd46405906288a40f965c4f6728c8b2a398434108afc2de6526594b80306da23a70da30596c674988e6ac800cfb127c32b781861bc370a477f3aa4a1f3293e083'
