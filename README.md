9eea1f176111101784f1c1ef987947cb04ace78b
Leah Brann <lbrann@gmail.com>
2014-04-23 18:49:13 -0700

    Added Edit and Delete for activity records

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 WellnessTracker
 ===============
+Application for creating and running wellness challenges with administrator-defined activities and points 
--- app/controllers/activity_records_controller.rb
+++ app/controllers/activity_records_controller.rb
@@ -14,8 +14,6 @@ class ActivityRecordsController < ApplicationController
 
 		@activities = Activity.all
 		@categories = Category.all
-		#@user = current_user
-		#@activity_record = @user.activity_records.create(params[:activityrecord])
 		@activity_record = ActivityRecord.create(activity_record_params)
 		@activity_record.user = current_user
 		if @activity_record.save
@@ -26,6 +24,23 @@ class ActivityRecordsController < ApplicationController
 
 	end
 
+	def destroy
+		@activity_record = ActivityRecord.find(params[:id])
+		@activity_record.destroy
+		redirect_to activity_records_path
+	end
+
+	def edit
+		@activity_record = ActivityRecord.find(params[:id])
+		@activities = Activity.all
+		@categories = Category.all
+	end
+
+	def update
+		@activity_record = ActivityRecord.find(params[:id])
+		@activity_record.update(activity_record_params)
+		redirect_to activity_records_path
+	end
 	
 
 
