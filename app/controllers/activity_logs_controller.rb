c5ebb98efd96f0d8824fa03e975690109a7c8342
Leah Brann <lbrann@gmail.com>
2014-05-11 20:55:35 -0700

    Grouped common actions into before_filters

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,4 +1,6 @@
 class ActivityLogsController < ApplicationController
+	before_filter :find_log, :only => [:copy, :show, :destroy, :edit, :update]
+	before_filter :setup_activities_and_categories, :only => [:copy, :edit, :new, :create]
 
 def index
 		@activity_logs = current_user.activity_logs.sort_by{|log|log.date}.reverse
@@ -7,15 +9,11 @@ end
 def new
 	@activity_log = ActivityLog.new
 	@activity_records = @activity_log.activity_records.build
-	@activities = Activity.order(title: :asc)
-	@categories = Category.order(title: :asc)
 end
 
 def create
 	@activity_log = ActivityLog.new(activity_log_params)
 	@activity_log.user = current_user
-	@activities = Activity.order(title: :asc)
-	@categories = Category.order(title: :asc)
 	@activity_log.create_records(params[:activity_log][:activity_ids])
 		if @activity_log.save
 			redirect_to activity_logs_path
@@ -25,24 +23,17 @@ def create
 end
 
 def show
-	@activity_log = ActivityLog.find(params[:id])
 end
 
 def destroy
-	@activity_log = ActivityLog.find(params[:id])
 	@activity_log.destroy
 	redirect_to activity_logs_path
 end
 
 def edit
-	@activity_log = ActivityLog.find(params[:id])
-	@activities = Activity.order(title: :asc)
-	@categories = Category.order(title: :asc)
-
 end
 
 def update
-	@activity_log = ActivityLog.find(params[:id])
 	@activity_log.update(activity_log_params)
 	@activity_log.activity_records.destroy_all
 	@activity_log.create_records(params[:activity_log][:activity_ids])
@@ -54,15 +45,21 @@ def update
 end
 
 def copy
-	@activity_log = ActivityLog.find(params[:id])
-	@activities = Activity.order(title: :asc)
-	@categories = Category.order(title: :asc)
 end
 
 	private
 
 	def activity_log_params
 		params.require(:activity_log).permit(:date, :user_id, :activity_records_attributes => [:activity_id => []])
+	end
+
+	def find_log
+		@activity_log = ActivityLog.find(params[:id])
+	end
+
+	def setup_activities_and_categories
+		@activities = Activity.order(title: :asc)
+		@categories = Category.order(title: :asc)
 	end
 
 end
